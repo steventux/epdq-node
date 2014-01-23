82afd4d3fd4643216f2a6117520d705aa52f2730
Steve Laing <steve.laing@gmail.com>
2014-01-23 10:20:36 +0000

    WIP EPDQ config

--- epdq.js
+++ epdq.js
@@ -1,43 +1,7 @@
-var Request = require('epdq/request'),
-    Response = require('epdq/response');
-
-var EPDQ = (function(){
-  var testMode = false,
-      shaIn,
-      shaOut,
-      pspId;
-
-  return {
-    // Export Request and Response classes in the EPDQ namespace.
-    Request : Request,
-    Response : Response,
-
-    // Module accessors/mutators
-    setTestMode : function(_testMode){
-      testMode = _testMode;
-    },
-    isTestMode : function(){
-      return testMode;
-    },
-    setShaIn : function(_shaIn){
-      shaIn = _shaIn;
-    },
-    getShaIn : function(){
-      return shaIn;
-    },
-    setShaOut : function(_shaOut){
-      shaOut = _shaOut;
-    },
-    getShaOut : function(){
-      return shaOut;
-    },
-    setPspId : function(_pspId){
-      pspId = _pspId;
-    },
-    getPspId : function(){
-      return pspId;
-    }
-  }
-})();
-
-module.exports = EPDQ;
+// TODO: Share state across modules.
+module.exports = {
+  testMode : false,
+  shaIn : null,
+  shaOut : null,
+  pspId : null,
+};
